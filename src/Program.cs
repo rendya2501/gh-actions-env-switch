0ddbed66866c3efcddae2aca16a18acf197512b7
rendya <60930906+rendya2501@users.noreply.github.com>
2025-02-20 07:04:36 +0000

    sd

--- src/Program.cs
+++ src/Program.cs
@@ -3,8 +3,15 @@ using System.IO;
 
 var exportedCount = new Random().Next(0, 2); // 0 または 1 をランダムに出力
 
-// GITHUB_ENV に値を書き込む
-using (var writer = new StreamWriter(Environment.GetEnvironmentVariable("GITHUB_ENV"), true))
+// NULLチェック
+var githubEnvPath = Environment.GetEnvironmentVariable("GITHUB_ENV");
+if (githubEnvPath == null)
 {
-    writer?.WriteLine($"EXPORTED_COUNT={exportedCount}");
+    throw new Exception("Environment.GetEnvironmentVariable(GITHUB_ENV) is null!!");
+}
+
+// GITHUB_ENV に値を書き込む
+using (var writer = new StreamWriter(githubEnvPath, true))
+{
+    writer.WriteLine($"EXPORTED_COUNT={exportedCount}");
 }
