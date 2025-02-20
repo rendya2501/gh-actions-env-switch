fddff65fed4add2d0c5efa85ab8a6ac70e88e2f8
rendya <60930906+rendya2501@users.noreply.github.com>
2025-02-20 06:19:03 +0000

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+var exportedCount = new Random().Next(0, 2); // 0 または 1 をランダムに出力
+
+// GITHUB_ENV に値を書き込む
+using (var writer = new StreamWriter(Environment.GetEnvironmentVariable("GITHUB_ENV"), true))
+{
+    writer.WriteLine($"EXPORTED_COUNT={exportedCount}");
+}
+
+Console.WriteLine($"Exported count: {exportedCount}");
