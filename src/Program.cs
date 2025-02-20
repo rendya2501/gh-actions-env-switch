f1fc5267dcdf8ac01a2dc69687495d9f8a69a661
rendya <60930906+rendya2501@users.noreply.github.com>
2025-02-20 06:37:07 +0000

    ss

--- src/Program.cs
+++ src/Program.cs
@@ -6,7 +6,5 @@ var exportedCount = new Random().Next(0, 2); // 0 または 1 をランダムに
 // GITHUB_ENV に値を書き込む
 using (var writer = new StreamWriter(Environment.GetEnvironmentVariable("GITHUB_ENV"), true))
 {
-    writer.WriteLine($"EXPORTED_COUNT={exportedCount}");
+    writer?.WriteLine($"EXPORTED_COUNT={exportedCount}");
 }
-
-Console.WriteLine($"Exported count: {exportedCount}");
